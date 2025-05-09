ca622559f6c31f4fb1cdea396f97e577636cc6ab
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:26:55 +0300

    Adress entity oluşturuldu. Field'ları oluşturuldu. version1

---
+++ src/main/java/com/example/erp/demo/model/Address.java
@@ -0,0 +1,58 @@
+package com.example.erp.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class Address extends BaseEntity{
+
+    @Column(name = "country",
+            length = 50,
+            nullable = false)
+    private String country;
+
+    @Column(name = "city",
+            length = 50,
+            nullable = false)
+    private String city;
+
+    @Column(name = "state_province",
+            length = 50,
+            nullable = false)
+    private String stateProvince;
+
+    @Column(name = "district",
+            length = 50,
+            nullable = false)
+    private String district;
+
+    @Column(name = "street_address",
+            length = 100)
+    private String streetAddress;
+
+    @Column(name = "address_line_two",
+            length = 100)
+    private String addressLineTwo;
+
+    @Column(name = "postal_code",
+            nullable = false)
+    private String postalCode;
+
+    @Column(name = "verified",
+            nullable = false)
+    private Boolean isVerified;
+
+
+    // relation
+//    private Customer customer;
+//    private Producer producer;
+
+}
