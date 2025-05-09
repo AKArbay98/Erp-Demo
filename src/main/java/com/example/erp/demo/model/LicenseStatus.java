7f3c51bf8924fe5e87801a8a51799958706cc43f
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:52:02 +0300

    License Status Name Type oluşturuldu ve tanımlandı.

---
+++ src/main/java/com/example/erp/demo/model/LicenseStatus.java
@@ -0,0 +1,17 @@
+package com.example.erp.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public enum LicenseStatus {
+
+    ACTIVE("ACTIVE"),
+    EXPIRED("EXPIRED"),
+    REVOKED("EXPIRED");
+
+    private final String licenseStatusName;
+}
