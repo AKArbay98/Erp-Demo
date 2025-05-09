1e836d1b27e7e4cc1efcccb187a71265c81ddab8
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:12:33 +0300

    Gender enum'ı oluşturuldu ve tanımlandı.

---
+++ src/main/java/com/example/erp/demo/model/Gender.java
@@ -0,0 +1,17 @@
+package com.example.erp.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public enum Gender {
+
+    MALE("M","Male"),
+    FEMALE("F", "Female");
+
+    private final String code;
+    private final String displayName;
+}
