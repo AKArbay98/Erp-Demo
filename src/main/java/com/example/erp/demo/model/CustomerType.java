d75bf4e729fcc1f1cd03cdabaed292bc859adbb3
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:19:24 +0300

    Customer Type enum'ı oluşturuldu ve tanımlandı.

---
+++ src/main/java/com/example/erp/demo/model/CustomerType.java
@@ -0,0 +1,16 @@
+package com.example.erp.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public enum CustomerType {
+
+    PERSONAL("Personal"),
+    CORPORATE("Corporate");
+
+    private final String customerType;
+}
