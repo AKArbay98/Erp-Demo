8e224e86b4429b51dd3bd43e127c6725004823e7
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:38:15 +0300

    Product Entity field'lar eklendi. version3-refactor1

--- src/main/java/com/example/erp/demo/model/Product.java
+++ src/main/java/com/example/erp/demo/model/Product.java
@@ -1,9 +1,14 @@
 package com.example.erp.demo.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "product")
@@ -11,8 +16,44 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @ToString
 @EqualsAndHashCode(callSuper = false)
 public class Product extends BaseEntity{
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "product-version")
+    private String productVersion;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "base-price")
+    private BigDecimal basePrice;
+
+    @Column(name = "discounted-price")
+    private BigDecimal discountedPrice;
+
+    @Column(name = "release-date")
+    private LocalDateTime releaseDate;
+
+    @Future
+    @Column(name = "support-expiration")
+    private LocalDate supportExpiration;
+
+    @Column(name = "customer-satisfaction")
+    private BigDecimal customerSatisfaction;
+
+    @Column(name = "comment")
+    private String comment;
+
+    // relation
+//    private List<License> licenses;
+//    private Producer producer;
+
+
 }
