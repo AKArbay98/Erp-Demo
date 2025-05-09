77a80ea7ce2daeeaed41bc6b62a02f94e4569955
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:00:17 +0300

    Customer Entity refactor edildi. version2-refactor1

--- src/main/java/com/example/erp/demo/model/Customer.java
+++ src/main/java/com/example/erp/demo/model/Customer.java
@@ -1,4 +1,18 @@
 package com.example.erp.demo.model;
 
-public class Customer {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class Customer extends BaseEntity{
+
 }
