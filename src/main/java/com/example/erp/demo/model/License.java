4e5fe3c304d07fc5aa39b0ae6851d0acdd32def8
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:01:34 +0300

    License Entity refactor edildi. version2-refactor1

--- src/main/java/com/example/erp/demo/model/License.java
+++ src/main/java/com/example/erp/demo/model/License.java
@@ -1,4 +1,17 @@
 package com.example.erp.demo.model;
 
-public class License {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "license")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(callSuper = false)
+public class License extends BaseEntity{
 }
