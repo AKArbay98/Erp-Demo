b18e9738db723aac4861abe1be017bc2dc098bde
AKArbay98 <alikarbay98@gmail.com>
2025-05-09 23:09:33 +0300

    Customer Entity field'lar eklendi. version3-refactor1

--- src/main/java/com/example/erp/demo/model/Customer.java
+++ src/main/java/com/example/erp/demo/model/Customer.java
@@ -1,8 +1,10 @@
 package com.example.erp.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+
 
 @Entity
 @Table(name = "customer")
@@ -10,9 +12,48 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @ToString
 @EqualsAndHashCode(callSuper = false)
 public class Customer extends BaseEntity{
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "dateOfBirth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "tc", nullable = false)
+    private String tc;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "photoUrl")
+    private String photoUrl;
+
+    @Column(name = "occupation")
+    private String occupation;
+
+    @Column(name = "language")
+    private String language;
+
+    // enums
+    // private Gender gender;
+
+
+    // private ContactMethod contactMethod;
+
+
+    //private CustomerType customerType;
+
+    //relation
+    // private Address address;
+    // private List<License> licenses;
 
 }
