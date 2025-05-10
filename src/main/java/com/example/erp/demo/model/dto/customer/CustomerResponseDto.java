0671d4e2376e141e7b4b48ecb2cc751405398d0d
AKArbay98 <alikarbay98@gmail.com>
2025-05-10 22:07:20 +0300

    CustomerRequestDto ve CustomerResponseDto'nun fieldlarını oluşturdum.

--- src/main/java/com/example/erp/demo/model/dto/customer/CustomerResponseDto.java
+++ src/main/java/com/example/erp/demo/model/dto/customer/CustomerResponseDto.java
@@ -1,4 +1,32 @@
 package com.example.erp.demo.model.dto.customer;
 
+import com.example.erp.demo.model.entity.address.Address;
+import com.example.erp.demo.model.entity.license.License;
+import com.example.erp.demo.model.enumeration.ContactMethod;
+import com.example.erp.demo.model.enumeration.CustomerType;
+import com.example.erp.demo.model.enumeration.Gender;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
 public class CustomerResponseDto {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String tc;
+    private String phone;
+    private String email;
+    private String photoUrl;
+    private String occupation;
+    private String language;
+    private Gender gender;
+    private ContactMethod contactMethod;
+    private CustomerType customerType;
+    private Address address;
+    private List<License> licenses;
 }
