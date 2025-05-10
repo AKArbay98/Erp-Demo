699e668dd21ebdca1eff4cdc745ab195ffeef85d
AKArbay98 <alikarbay98@gmail.com>
2025-05-10 22:28:31 +0300

    LicenseRequestDto ve LicenseResponseDto'nun fieldlarını oluşturdum.

--- src/main/java/com/example/erp/demo/model/dto/license/LicenseRequestDto.java
+++ src/main/java/com/example/erp/demo/model/dto/license/LicenseRequestDto.java
@@ -1,4 +1,32 @@
 package com.example.erp.demo.model.dto.license;
 
+import com.example.erp.demo.model.entity.customer.Customer;
+import com.example.erp.demo.model.entity.product.Product;
+import com.example.erp.demo.model.enumeration.LicenseStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class LicenseRequestDto {
+
+    private String licenseKey;
+
+    private String licenseName;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private Boolean autoRenew;
+
+    private LicenseStatus licenseStatus;
+
+    private String licenseDetail;
+
+    private Customer customer;
+
+    private Product product;
 }
