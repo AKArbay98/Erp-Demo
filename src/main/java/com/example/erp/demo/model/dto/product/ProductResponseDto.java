ac0d8e3d299f89da4a2c24ee2f5e65565d53190b
AKArbay98 <alikarbay98@gmail.com>
2025-05-10 22:17:45 +0300

    ProductRequestDto ve ProductResponseDto'nun fieldlarını oluşturdum.

--- src/main/java/com/example/erp/demo/model/dto/product/ProductResponseDto.java
+++ src/main/java/com/example/erp/demo/model/dto/product/ProductResponseDto.java
@@ -1,4 +1,27 @@
 package com.example.erp.demo.model.dto.product;
 
+import com.example.erp.demo.model.entity.address.Address;
+import com.example.erp.demo.model.entity.product.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class ProductResponseDto {
+
+    private String name;
+
+    private String description;
+
+    private String contactEmail;
+
+    private String contactPhone;
+
+    private String websiteUrl;
+
+    private List<Product> products;
+
+    private Address address;
 }
