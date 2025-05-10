c95f7e4e67480a4252ee3f74d961fb6044f7c1ac
AKArbay98 <alikarbay98@gmail.com>
2025-05-10 21:58:37 +0300

    AddressRequestDto ve AddressResponseDto'nun fieldlarını oluşturdum.

--- src/main/java/com/example/erp/demo/model/dto/address/AddressResponseDto.java
+++ src/main/java/com/example/erp/demo/model/dto/address/AddressResponseDto.java
@@ -1,4 +1,22 @@
 package com.example.erp.demo.model.dto.address;
 
+import com.example.erp.demo.model.entity.customer.Customer;
+import com.example.erp.demo.model.entity.producer.Producer;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AddressResponseDto {
+
+    private String country;
+    private String city;
+    private String stateProvince;
+    private String district;
+    private String streetAddress;
+    private String addressLineTwo;
+    private String postalCode;
+    private Boolean isVerified;
+    private Customer customer;
+    private Producer producer;
 }
