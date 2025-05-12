6d0347997f1151a8125514352bd0713be7082a0e
AKArbay98 <alikarbay98@gmail.com>
2025-05-12 21:16:31 +0300

    ProductMapperImpl'de toProductDtoFromProductEntity ve toProductEntityFromProductDto metotlarını implemente ettim. Refactor edeceğim.

--- src/main/java/com/example/erp/demo/mapper/product/ProductMapperImpl.java
+++ src/main/java/com/example/erp/demo/mapper/product/ProductMapperImpl.java
@@ -3,15 +3,50 @@ package com.example.erp.demo.mapper.product;
 import com.example.erp.demo.model.dto.product.ProductRequestDto;
 import com.example.erp.demo.model.dto.product.ProductResponseDto;
 import com.example.erp.demo.model.entity.product.Product;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class ProductMapperImpl implements ProductMapper{
+
     @Override
     public ProductResponseDto toProductDtoFromProductEntity(Product product) {
-        return null;
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setName(product.getName());
+        productResponseDto.setProductVersion(product.getProductVersion());
+        productResponseDto.setDescription(product.getDescription());
+        productResponseDto.setSku(product.getSku());
+        productResponseDto.setBasePrice(product.getBasePrice());
+        productResponseDto.setDiscountedPrice(product.getDiscountedPrice());
+        productResponseDto.setReleaseDate(product.getReleaseDate());
+        productResponseDto.setSupportExpiration(product.getSupportExpiration());
+        productResponseDto.setCustomerSatisfaction(product.getCustomerSatisfaction());
+        productResponseDto.setComment(product.getComment());
+
+        // relation lara da bak
+
+        return productResponseDto;
     }
 
     @Override
     public Product toProductEntityFromProductDto(ProductRequestDto productRequestDto) {
-        return null;
+        if(productRequestDto == null){
+            return null;
+        }
+        Product product = new Product();
+        product.setName(productRequestDto.getName());
+        product.setProductVersion(productRequestDto.getProductVersion());
+        product.setDescription(productRequestDto.getDescription());
+        product.setSku(productRequestDto.getSku());
+        product.setBasePrice(productRequestDto.getBasePrice());
+        product.setDiscountedPrice(productRequestDto.getDiscountedPrice());
+        product.setReleaseDate(productRequestDto.getReleaseDate());
+        product.setSupportExpiration(productRequestDto.getSupportExpiration());
+        product.setCustomerSatisfaction(productRequestDto.getCustomerSatisfaction());
+        product.setComment(productRequestDto.getComment());
+
+        // relation lara da bak
+
+        return product;
     }
 }
