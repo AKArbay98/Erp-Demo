12a58926fb445ae0292825622b776d99de7d392b
AKArbay98 <alikarbay98@gmail.com>
2025-05-13 03:58:58 +0300

    ContactController refactor edildi.

--- src/main/java/com/example/erp/demo/controller/customer/CustomerController.java
+++ src/main/java/com/example/erp/demo/controller/customer/CustomerController.java
@@ -2,14 +2,12 @@ package com.example.erp.demo.controller.customer;
 
 import com.example.erp.demo.model.dto.customer.CustomerRequestDto;
 import com.example.erp.demo.model.dto.customer.CustomerResponseDto;
-import com.example.erp.demo.model.entity.customer.Customer;
 import com.example.erp.demo.service.customer.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +16,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customer")
-@RequiredArgsConstructor
 public class CustomerController {
 
     private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @Operation(summary = "Retrieve all customers")
     @ApiResponse(responseCode = "200", description = "List of customers returned",
@@ -45,7 +46,7 @@ public class CustomerController {
         return ResponseEntity.ok(customerService.getCustomerById(customerId));
     }
 
-    @Operation(summary = "Create a new customer   ")
+    @Operation(summary = "Create a new customer")
     @ApiResponses({
             @ApiResponse(responseCode = "201", description = "Customer created successfully"),
             @ApiResponse(responseCode = "400", description = "Invalid request data",
@@ -59,15 +60,12 @@ public class CustomerController {
 
     @Operation(summary = "Get customer by license key")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "License details returned"),
-            @ApiResponse(responseCode = "404", description = "License not found")
+            @ApiResponse(responseCode = "200", description = "Customer details returned"),
+            @ApiResponse(responseCode = "404", description = "Customer not found")
     })
-    @GetMapping("/{licenseKey}")
+    @GetMapping("/license/{licenseKey}")
     public ResponseEntity<CustomerResponseDto> getCustomerByLicenseKey(@PathVariable String licenseKey){
         return ResponseEntity.ok(customerService.getCustomerByLicenseKey(licenseKey));
     }
-
-
-
 
 }
