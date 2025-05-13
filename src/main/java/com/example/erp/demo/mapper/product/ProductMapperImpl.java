18f249b1af86c944538e8ca0c9bddccb3eee92c7
AKArbay98 <alikarbay98@gmail.com>
2025-05-13 04:08:41 +0300

    ProductMapperImpl refactor edildi.

--- src/main/java/com/example/erp/demo/mapper/product/ProductMapperImpl.java
+++ src/main/java/com/example/erp/demo/mapper/product/ProductMapperImpl.java
@@ -1,13 +1,25 @@
 package com.example.erp.demo.mapper.product;
 
+import com.example.erp.demo.mapper.license.LicenseMapper;
+import com.example.erp.demo.mapper.producer.ProducerMapper;
 import com.example.erp.demo.model.dto.product.ProductRequestDto;
 import com.example.erp.demo.model.dto.product.ProductResponseDto;
 import com.example.erp.demo.model.entity.product.Product;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class ProductMapperImpl implements ProductMapper{
+
+    private final LicenseMapper licenseMapper;
+    private final ProducerMapper producerMapper;
+
+    public ProductMapperImpl(@Lazy LicenseMapper licenseMapper,
+                             @Lazy ProducerMapper producerMapper) {
+        this.licenseMapper = licenseMapper;
+        this.producerMapper = producerMapper;
+    }
 
     @Override
     public ProductResponseDto toProductDtoFromProductEntity(Product product) {
@@ -22,9 +34,6 @@ public class ProductMapperImpl implements ProductMapper{
         productResponseDto.setSupportExpiration(product.getSupportExpiration());
         productResponseDto.setCustomerSatisfaction(product.getCustomerSatisfaction());
         productResponseDto.setComment(product.getComment());
-
-        // relation lara da bak
-
         return productResponseDto;
     }
 
@@ -44,9 +53,6 @@ public class ProductMapperImpl implements ProductMapper{
         product.setSupportExpiration(productRequestDto.getSupportExpiration());
         product.setCustomerSatisfaction(productRequestDto.getCustomerSatisfaction());
         product.setComment(productRequestDto.getComment());
-
-        // relation lara da bak
-
         return product;
     }
 }
