66d328cc563f457556e4f31ee5e088598be3b492
AKArbay98 <alikarbay98@gmail.com>
2025-05-13 03:54:58 +0300

    LicenseRequestDto ve LicenseResponseDto refactor edildi.

--- src/main/java/com/example/erp/demo/model/dto/license/LicenseRequestDto.java
+++ src/main/java/com/example/erp/demo/model/dto/license/LicenseRequestDto.java
@@ -2,8 +2,6 @@ package com.example.erp.demo.model.dto.license;
 
 import com.example.erp.demo.model.dto.customer.CustomerRequestDto;
 import com.example.erp.demo.model.dto.product.ProductRequestDto;
-import com.example.erp.demo.model.entity.customer.Customer;
-import com.example.erp.demo.model.entity.product.Product;
 import com.example.erp.demo.model.enumeration.LicenseStatus;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,20 +13,12 @@ import java.time.LocalDate;
 public class LicenseRequestDto {
 
     private String licenseKey;
-
     private String licenseName;
-
     private LocalDate startDate;
-
     private LocalDate endDate;
-
     private Boolean autoRenew;
-
     private LicenseStatus licenseStatus;
-
     private String licenseDetail;
-
     private CustomerRequestDto customer;
-
     private ProductRequestDto product;
 }
--- src/main/java/com/example/erp/demo/model/dto/license/LicenseResponseDto.java
+++ src/main/java/com/example/erp/demo/model/dto/license/LicenseResponseDto.java
@@ -2,8 +2,6 @@ package com.example.erp.demo.model.dto.license;
 
 import com.example.erp.demo.model.dto.customer.CustomerResponseDto;
 import com.example.erp.demo.model.dto.product.ProductResponseDto;
-import com.example.erp.demo.model.entity.customer.Customer;
-import com.example.erp.demo.model.entity.product.Product;
 import com.example.erp.demo.model.enumeration.LicenseStatus;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,20 +13,12 @@ import java.time.LocalDate;
 public class LicenseResponseDto {
 
     private String licenseKey;
-
     private String licenseName;
-
     private LocalDate startDate;
-
     private LocalDate endDate;
-
     private Boolean autoRenew;
-
     private LicenseStatus licenseStatus;
-
     private String licenseDetail;
-
     private CustomerResponseDto customer;
-
     private ProductResponseDto product;
 }
