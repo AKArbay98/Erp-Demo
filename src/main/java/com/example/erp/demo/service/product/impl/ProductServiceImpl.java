6e022a5b558ebf5d528459073402ca44cd8e2357
AKArbay98 <alikarbay98@gmail.com>
2025-05-13 04:10:55 +0300

    ProductServiceImpl de getProductByLicenseKey metodu implemente edildi.

--- src/main/java/com/example/erp/demo/service/product/impl/ProductServiceImpl.java
+++ src/main/java/com/example/erp/demo/service/product/impl/ProductServiceImpl.java
@@ -2,7 +2,7 @@ package com.example.erp.demo.service.product.impl;
 
 import com.example.erp.demo.exception.LicenseNotFoundException;
 import com.example.erp.demo.exception.ProductNotFoundException;
-import com.example.erp.demo.mapper.product.ProductMapperImpl;
+import com.example.erp.demo.mapper.product.ProductMapper;
 import com.example.erp.demo.model.dto.product.ProductRequestDto;
 import com.example.erp.demo.model.dto.product.ProductResponseDto;
 import com.example.erp.demo.model.entity.product.Product;
@@ -18,15 +18,15 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
-    private final ProductMapperImpl productMapper;
+    private final ProductMapper productMapper;
     private final LicenseRepository licenseRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository,
-                              ProductMapperImpl productMapper, LicenseRepository licenseRepository) {
+    public ProductServiceImpl(ProductRepository productRepository, ProductMapper productMapper, LicenseRepository licenseRepository) {
         this.productRepository = productRepository;
         this.productMapper = productMapper;
         this.licenseRepository = licenseRepository;
     }
+
 
     @Override
     public List<ProductResponseDto> getAllProduct() {
@@ -59,10 +59,11 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductResponseDto getProductByLicenseKey(String licenseKey) {
-        return licenseRepository.findByLicenseKey(licenseKey)
-                .orElseThrow(() ->
-                        new LicenseNotFoundException("License not found: " + licenseKey))
-                .getProduct();
+        return productMapper.toProductDtoFromProductEntity(
+                licenseRepository.findByLicenseKey(licenseKey)
+                        .orElseThrow(() -> new LicenseNotFoundException("License not found: " + licenseKey))
+                        .getProduct()
+        );
     }
 
 
