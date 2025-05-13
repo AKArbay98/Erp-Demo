a9f18686606d3714f1313e9ac7da5d5e1387542c
AKArbay98 <alikarbay98@gmail.com>
2025-05-13 03:47:08 +0300

    Role class'ı oluşturuldu.

---
+++ src/main/java/com/example/erp/demo/model/enumeration/Role.java
@@ -0,0 +1,16 @@
+//package com.example.erp.demo.model.enumeration;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.ToString;
+//
+//@Getter
+//@AllArgsConstructor
+//@ToString
+//public enum Role {
+//
+//    USER("USER"),
+//    ADMIN("ADMIN");
+//
+//    private final String roleName;
+//}
