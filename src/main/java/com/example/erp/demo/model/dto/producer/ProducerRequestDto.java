0a6453c90d08a6620851a031a999eddcfa542174
AKArbay98 <alikarbay98@gmail.com>
2025-05-13 04:05:25 +0300

    ProducerResponseDto ve ProducerRequestDto refactor edildi.

--- src/main/java/com/example/erp/demo/model/dto/producer/ProducerRequestDto.java
+++ src/main/java/com/example/erp/demo/model/dto/producer/ProducerRequestDto.java
@@ -2,8 +2,6 @@ package com.example.erp.demo.model.dto.producer;
 
 import com.example.erp.demo.model.dto.address.AddressRequestDto;
 import com.example.erp.demo.model.dto.product.ProductRequestDto;
-import com.example.erp.demo.model.entity.address.Address;
-import com.example.erp.demo.model.entity.product.Product;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,16 +12,10 @@ import java.util.List;
 public class ProducerRequestDto {
 
     private String name;
-
     private String description;
-
     private String contactEmail;
-
     private String contactPhone;
-
     private String websiteUrl;
-
     private List<ProductRequestDto> products;
-
     private AddressRequestDto address;
 }
--- src/main/java/com/example/erp/demo/model/dto/producer/ProducerResponseDto.java
+++ src/main/java/com/example/erp/demo/model/dto/producer/ProducerResponseDto.java
@@ -2,28 +2,18 @@ package com.example.erp.demo.model.dto.producer;
 
 import com.example.erp.demo.model.dto.address.AddressResponseDto;
 import com.example.erp.demo.model.dto.product.ProductResponseDto;
-import com.example.erp.demo.model.entity.address.Address;
-import com.example.erp.demo.model.entity.product.Product;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 
-
 @Getter
 @Setter
 public class ProducerResponseDto {
-
     private String name;
-
     private String description;
-
     private String contactEmail;
-
     private String contactPhone;
-
     private String websiteUrl;
-
     private List<ProductResponseDto> products;
-
     private AddressResponseDto address;
 }
