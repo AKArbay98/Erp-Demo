eb4aa07e7ee4e1ea733802e6c656eefd7d9d4265
AKArbay98 <alikarbay98@gmail.com>
2025-05-13 03:53:47 +0300

    LicenseMapperImpl refactor edildi.

--- src/main/java/com/example/erp/demo/mapper/license/LicenseMapper.java
+++ src/main/java/com/example/erp/demo/mapper/license/LicenseMapper.java
@@ -1,4 +1,10 @@
 package com.example.erp.demo.mapper.license;
 
+import com.example.erp.demo.model.dto.license.LicenseRequestDto;
+import com.example.erp.demo.model.dto.license.LicenseResponseDto;
+import com.example.erp.demo.model.entity.license.License;
+
 public interface LicenseMapper {
+    LicenseResponseDto toLicenseDtoFromLicenseEntity(License license);
+    License toLicenseEntityFromLicenseDto(LicenseRequestDto licenseRequestDto);
 }
--- src/main/java/com/example/erp/demo/mapper/license/LicenseMapperImpl.java
+++ src/main/java/com/example/erp/demo/mapper/license/LicenseMapperImpl.java
@@ -1,17 +1,27 @@
 package com.example.erp.demo.mapper.license;
 
+import com.example.erp.demo.mapper.customer.CustomerMapper;
+import com.example.erp.demo.mapper.product.ProductMapper;
 import com.example.erp.demo.model.dto.license.LicenseRequestDto;
 import com.example.erp.demo.model.dto.license.LicenseResponseDto;
 import com.example.erp.demo.model.entity.license.License;
-import com.example.erp.demo.model.enumeration.LicenseStatus;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 
 @Component
 public class LicenseMapperImpl implements LicenseMapper{
 
+    private final CustomerMapper customerMapper;
+    private final ProductMapper productMapper;
 
+    public LicenseMapperImpl(@Lazy CustomerMapper customerMapper,
+                             @Lazy ProductMapper productMapper) {
+        this.customerMapper = customerMapper;
+        this.productMapper = productMapper;
+    }
+
+    @Override
     public LicenseResponseDto toLicenseDtoFromLicenseEntity(License license){
         LicenseResponseDto licenseResponseDto = new LicenseResponseDto();
         licenseResponseDto.setLicenseKey(license.getLicenseKey());
@@ -21,13 +31,18 @@ public class LicenseMapperImpl implements LicenseMapper{
         licenseResponseDto.setAutoRenew(license.getAutoRenew());
         licenseResponseDto.setLicenseStatus(license.getLicenseStatus());
         licenseResponseDto.setLicenseDetail(license.getLicenseDetail());
-
-        // relation lara da bak
-
+        if (license.getCustomer() != null) {
+            licenseResponseDto.setCustomer(customerMapper
+                    .toCustomerDtoFromCustomerEntity(license.getCustomer()));
+        }
+        if (license.getProduct() != null) {
+            licenseResponseDto.setProduct(productMapper
+                    .toProductDtoFromProductEntity(license.getProduct()));
+        }
         return licenseResponseDto;
     }
 
-
+    @Override
     public License toLicenseEntityFromLicenseDto(LicenseRequestDto licenseRequestDto){
         if(licenseRequestDto == null){
             return null;
@@ -40,9 +55,14 @@ public class LicenseMapperImpl implements LicenseMapper{
         license.setAutoRenew(license.getAutoRenew());
         license.setLicenseStatus(license.getLicenseStatus());
         license.setLicenseDetail(license.getLicenseDetail());
-
-        // relation lara da bak
-
+        if (licenseRequestDto.getCustomer() != null) {
+            license.setCustomer(customerMapper
+                    .toCustomerEntityFromCustomerDto(licenseRequestDto.getCustomer()));
+        }
+        if (licenseRequestDto.getProduct() != null) {
+            license.setProduct(productMapper
+                    .toProductEntityFromProductDto(licenseRequestDto.getProduct()));
+        }
         return license;
     }
 
